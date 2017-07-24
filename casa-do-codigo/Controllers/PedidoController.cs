b20248af6c98bfab89c4abbab6987bb1585f021c
Rafael Viegas <rafael.viegas@arpagus.com.br>
2017-07-24 11:28:16 -0300

    01. Criando aplicação ASP.NET Core MVC

---
+++ casa-do-codigo/Controllers/PedidoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace casa_do_codigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public IActionResult Carrossel()
+        {
+            return View();
+        }
+
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+
+        public IActionResult Resumo()
+        {
+            return View();
+        }
+    }
+}
